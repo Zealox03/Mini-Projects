4a9d48058dc58535d038c26f5e984ccc50639c55
dylan <1211103428@student.mmu.edu.my>
2025-11-18 22:53:21 +0800

    test

--- Practice_Day_Two.py
+++ Practice_Day_Two.py
@@ -3,34 +3,34 @@
 # and overrides the withdraw() method to prevent withdrawals that would cause the balance to fall below $100. 
 
 class BankAccount:
-    def __init__(self,initial_balance = 0):
+    def __init__(self,name,initial_balance=0):
+        self.name = name
         self.balance = initial_balance
 
     def deposit(self,amount):
         self.balance += amount
     
     def withdraw(self,amount):
-        if self.balance >= amount:
+        if self.balance > amount:
             self.balance -= amount
         else:
             print("Insufficient amount")
-
+    
     def show(self):
-        print(f"Remaining balance: {self.balance}") 
-
+        print(f"Username: {self.name}")
+        print(f"Balance: {self.balance}")
+    
 class SavingsAccount(BankAccount):
-    def withdraw(self,amount):
-        super().__init__(self,initial_balance=0)
-
+    def __init__(self,name,initial_balance):
+        super().__init__(name,initial_balance)
+    
     def withdraw(self,amount):
         if self.balance - amount >= 100:
             self.balance -= amount
         else:
-            print("Insuffecient balance")
+            print(f"Balance must not fall below $100")
 
-cimb_1 = SavingsAccount(100)
-cimb_1.show()
-cimb_1.deposit(100)
-cimb_1.show()
-cimb_1.withdraw(50)
-cimb_1.show()
+dylan_cimb = SavingsAccount("Dylan",100)
+dylan_cimb.deposit(500)
+dylan_cimb.withdraw(750)
+dylan_cimb.show()
